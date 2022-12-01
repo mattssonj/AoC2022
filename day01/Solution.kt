98f15f21ce65e7121dda714c83046d72a927d2fa
Joakim Mattsson <joamat91@gmail.com>
2022-12-01 07:13:50 +0100

    First draft of day 1

--- day01/Solution.kt
+++ day01/Solution.kt
@@ -2,22 +2,36 @@ import java.io.File
 
 fun main() {
     when (System.getenv("part")) {
-        "part2" -> println(solution2(readInput("day05/input.txt")))
-        else -> println(solution1(readInput("day05/input.txt")))
+        "part2" -> println(solution2(readInput("input.txt")))
+        else -> println(solution1(readInput("input.txt")))
     }
 }
 
-fun solution1(points: Points): Int {
- return 1
+fun solution1(lines: List<String>): Int {
+    var highest = 0
+    var current = 0
+    lines.forEach {
+        if (it.isBlank()) {
+            if (highest < current) highest = current
+            current = 0
+        } else current += it.toInt()
+    }
+    return highest
 }
 
-fun solution2(points: Points): Int {
-    return 2
+fun solution2(lines: List<String>): Int {
+    val calories = mutableListOf<Int>()
+    var current = 0
+    lines.forEachIndexed { index, s ->
+        if (s.isBlank()) {
+            calories.add(current)
+            current = 0
+        } else if (index + 1 == lines.size) {
+            current += s.toInt()
+            calories.add(current)
+        } else current += s.toInt()
+    }
+    return calories.sortedDescending().take(3).sum()
 }
 
-fun readInput(path: String): Points = File(path).readLines()
-    .map { it.split(" -> ").flatMap { it.split(",").chunked(2).map { Pair(it[0].toInt(), it[1].toInt()) } } }
-    .map { Pair(it[0], it[1]) }
-
-typealias Points = List<Pair<Point, Point>>
-typealias Point = Pair<Int, Int>
+fun readInput(path: String): List<String> = File(path).readLines()
