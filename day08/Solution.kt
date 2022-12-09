0a69f7ed1a7977e46239ba8504511c835dfbc64e
Joakim Mattsson <joamat91@gmail.com>
2022-12-09 11:49:41 +0100

    Day 8 part 2

--- day08/Solution.kt
+++ day08/Solution.kt
@@ -10,29 +10,30 @@ fun main() {
 }
 
 fun solution1(lines: List<String>): Int {
-    val result = isVisible(lines).filter { it }.size
+    val result = calc(lines).filter { (seen, _) -> seen }.size
     val edges = lines.size * 2 + lines.first().length * 2 - 4
     return result + edges
 }
 
-fun isVisible(map: List<String>): List<Boolean> {
+fun solution2(lines: List<String>): Int {
+    return calc(lines).maxOf { (_, numberOfTrees) -> numberOfTrees }
+}
+
+fun calc(map: List<String>): List<Pair<Boolean, Int>> {
     val horizontalRange = (1 until map.first().length - 1)
     val verticalRange = (1 until map.size - 1)
 
     val result = verticalRange.flatMap { vertical ->
         horizontalRange.map { horizontal ->
             val currentTree = Pair(vertical, horizontal)
-            val isVisibleLeft = isVisibleInHorizontalDirection(map, (0 until horizontal), currentTree)
-            val isVisibleRight = isVisibleInHorizontalDirection(map, (map.size - 1 downTo horizontal + 1), currentTree)
-            val isVisibleUp = isVisibleInVerticalDirection(map, (0 until vertical), currentTree)
-            val isVisibleDown =
-                isVisibleInVerticalDirection(map, (map.first().length - 1 downTo vertical + 1), currentTree)
+            val isVisibleLeft = isVisibleInHorizontalDirection(map, (horizontal - 1 downTo 0), currentTree)
+            val isVisibleRight = isVisibleInHorizontalDirection(map, (horizontal + 1 until map.first().length), currentTree)
+            val isVisibleUp = isVisibleInVerticalDirection(map, (vertical - 1 downTo 0), currentTree)
+            val isVisibleDown = isVisibleInVerticalDirection(map, (vertical + 1 until map.size), currentTree)
 
-            val isVisible = isVisibleRight or isVisibleLeft or isVisibleUp or isVisibleDown
-            val currentSize = map[vertical][horizontal].toInteger()
-            println("currentTree = ${currentTree} with size = $currentSize " + if (isVisible) "is visible" else "is not visible")
-            println("DEBUG: left = $isVisibleLeft, right = $isVisibleRight, up = $isVisibleUp, down = $isVisibleDown")
-            isVisible
+            val isVisible = isVisibleRight.first or isVisibleLeft.first or isVisibleUp.first or isVisibleDown.first
+            val treesSeen = isVisibleRight.second * isVisibleLeft.second * isVisibleUp.second * isVisibleDown.second
+            Pair(isVisible, treesSeen)
         }
     }
     return result
@@ -40,27 +41,26 @@ fun isVisible(map: List<String>): List<Boolean> {
 
 private fun Char.toInteger() = this.toString().toInt()
 
-fun isVisibleInVerticalDirection(map: List<String>, range: IntProgression, tree: Pair<Int, Int>): Boolean {
+fun isVisibleInVerticalDirection(map: List<String>, range: IntProgression, tree: Pair<Int, Int>): Pair<Boolean, Int> {
     val (vertical, horizontal) = tree
     val currentTreeSize = map[vertical][horizontal].toInteger()
-    val anyBigger = range.firstOrNull {
-        map[it][horizontal].toInteger() >= currentTreeSize
+    val anyBigger = range.map {
+        if (map[it][horizontal].toInteger() >= currentTreeSize) null else 1
     }
-    return anyBigger == null
+
+    val containsNull = anyBigger.contains(null)
+    return Pair(!containsNull, anyBigger.takeWhile { it != null }.size + if (containsNull) 1 else 0)
 }
 
-fun isVisibleInHorizontalDirection(map: List<String>, range: IntProgression, tree: Pair<Int, Int>): Boolean {
+fun isVisibleInHorizontalDirection(map: List<String>, range: IntProgression, tree: Pair<Int, Int>): Pair<Boolean, Int> {
     val (vertical, horizontal) = tree
     val currentTreeSize = map[vertical][horizontal].toInteger()
-    val anyBigger = range.firstOrNull {
-        map[vertical][it].toInteger() >= currentTreeSize
+
+    val anyBigger = range.map {
+        if (map[vertical][it].toInteger() >= currentTreeSize) null else 1
     }
-    return anyBigger == null
-}
-
-
-fun solution2(lines: List<String>): Long {
-    return 2
+    val containsNull = anyBigger.contains(null)
+    return Pair(!containsNull, anyBigger.takeWhile { it != null }.size + if (containsNull) 1 else 0)
 }
 
 fun readInput(path: String): List<String> = File(path).readLines()
